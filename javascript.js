9641afbb6eeff11e79b812404a31b479d5fbdab0
BhuvaneswariSaravanakumar <126410696+BhuvaneswariSaravanakumar@users.noreply.github.com>
2023-04-09 22:59:24 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function bb(){
+    var Firstname = document.getElementById("fname").value;
+    console.log(Firstname);
+    var Middlename = document.getElementById("mname").value;
+    console.log(Middlename);
+    var Lastname = document.getElementById("lname").value;
+    console.log(Lastname);
+    var res = document.getElementById("mail").value;
+    console.log(res);
+}
